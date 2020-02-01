69386bdf2c5784678b5c8c2951b507772d3de776
jun <wjxajh@foxmail.com>
2020-02-01 18:50:39 +0800

    斐波那契；爬楼梯

---
+++ java-algorithm/src/com/jun/a_常用算法/动态规划.java
@@ -0,0 +1,13 @@
+package com.jun.a_常用算法;
+
+import com.jun.斐波那契数列_509.斐波那契数列_509;
+
+/**
+ * @author Jun
+ * @date 2020/2/1 下午6:25
+ */
+public class 动态规划 {
+    public static void main(String[] args) {
+        new 斐波那契数列_509();
+    }
+}
---
+++ java-algorithm/src/com/jun/零钱兑换_322/零钱兑换_322.java
@@ -0,0 +1,29 @@
+package com.jun.零钱兑换_322;
+
+/**
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ * 示例 2:
+ * <p>
+ * 输入: coins = [2], amount = 3
+ * 输出: -1
+ * 说明:
+ * 你可以认为每种硬币的数量是无限的。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/coin-change
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jun
+ * @date 2020/2/1 下午6:42
+ */
+public class 零钱兑换_322 {
+    public int coinChange(int[] coins, int amount) {
+
+    }
+}
