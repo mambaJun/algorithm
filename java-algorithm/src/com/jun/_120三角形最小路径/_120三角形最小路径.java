ae344cb999466a34975052ee7743059c111f2fd0
王钧钧 <wangjunjun@njbandou.onaliyun.com>
2020-05-20 22:38:03 +0800

    add

--- java-algorithm/src/com/jun/_120三角形最小路径/_120三角形最小路径.java
+++ java-algorithm/src/com/jun/_120三角形最小路径/_120三角形最小路径.java
@@ -1,5 +1,7 @@
 package com.jun._120三角形最小路径;
 
+import com.jun.util.ArrayUtil;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -27,36 +29,93 @@ import java.util.List;
  * @date 2020/5/17 下午11:25
  */
 public class _120三角形最小路径 {
+
+    /**
+     * 动态规划:
+     * 优化
+     * 创建一个一维数组
+     *
+     * @param triangle
+     * @return
+     */
     public static int minimumTotal(List<List<Integer>> triangle) {
+        if (!isValid(triangle)) {
+            return 0;
+        }
+
+        int column = triangle.size();
+        int row = triangle.get(column - 1).size();
+
+        int[] dp = new int[row];
+        dp[0] = triangle.get(0).get(0);
+
+        int pre = 0;
+        int curr = 0;
+        for (int c = 1; c < column; c++) {
+            List<Integer> rows = triangle.get(c);
+            for (int r = 0; r < rows.get(c); r++) {
+                int value = rows.get(r);
+                curr = dp[r];
+                if (r == 0) {
+                    dp[r] = curr + rows.get(r);
+                } else if (r == c) {
+                    dp[r] = pre + rows.get(r);
+                } else {
+                    dp[r] = Math.min(pre, curr) + value;
+                }
+                pre = curr;
+            }
+        }
+
+        return getMin(dp);
+    }
+
+    /**
+     * 动态规划:
+     * 创建一个二维数组,同样创建一个类型这个一样的下三角形,自顶向下,数组的每个位置代表它的最小值,这样最下方的一层就是最终结果,只要从最后一行找出最小值即可
+     *
+     * @param triangle
+     * @return
+     */
+    public static int minimumTotal_old(List<List<Integer>> triangle) {
+        if (!isValid(triangle)) {
+            return 0;
+        }
 
         int column = triangle.size();
         int row = triangle.get(column - 1).size();
         int[][] dp = new int[row][column];
-/*
-相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
-现在的相邻接点找错了,按照这个定义来改
-*/
-        for (int c = 0; c < column; c++) {
-            for (int r = 0; r < triangle.get(c).size(); r++) {
-//                System.out.println(r + " - " + c);
-                // 第一行
-                if (c == 0) {
-                    dp[r][c] = triangle.get(r).get(c);
-                    break;
-                }
-                int lastColumnSize = c - 1;
-                int lastRowSize = triangle.get(lastColumnSize).size() - 1;
-                Integer currentValue = triangle.get(c).get(r);
-                // 每一行的第一个
+        dp[0][0] = triangle.get(0).get(0);
+
+        for (int c = 1; c < column; c++) {
+            List<Integer> integers = triangle.get(c);
+            for (int r = 0; r < integers.size(); r++) {
+                int value = integers.get(r);
                 if (r == 0) {
-                    dp[r][c] = dp[lastRowSize][lastColumnSize] + currentValue;
+                    dp[r][c] = dp[r][c - 1] + value;
                     continue;
                 }
-                dp[r][c] = Math.min(dp[r - 1][c], dp[lastRowSize][lastColumnSize] + currentValue);
+                if (r == c) {
+                    dp[r][c] = dp[r - 1][c - 1] + value;
+                    continue;
+                }
+                dp[r][c] = Math.min(dp[r][c - 1], dp[r - 1][c - 1]) + value;
             }
         }
 
-        return dp[row - 1][column - 1];
+        return getMin(dp[column - 1]);
+    }
+
+    private static boolean isValid(List<List<Integer>> triangle) {
+        return triangle != null || triangle.size() > 0;
+    }
+
+    private static int getMin(int[] dp) {
+        int min = dp[0];
+        for (int i = 0; i < dp.length; i++) {
+            min = Math.min(min, dp[i]);
+        }
+        return min;
     }
 
     public static void main(String[] args) {
@@ -68,6 +127,6 @@ public class _120三角形最小路径 {
         triangle.add(Arrays.asList(-1));
         triangle.add(Arrays.asList(2, 3));
         triangle.add(Arrays.asList(1, -1, -3));
-        System.out.println(minimumTotal(triangle));
+        System.out.println(minimumTotal_old(triangle));
     }
 }
---
+++ java-algorithm/src/com/jun/_139单词拆分/_139单词拆分.java
@@ -0,0 +1,69 @@
+package com.jun._139单词拆分;
+
+import java.util.*;
+
+/**
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * <p>
+ * 说明：
+ * <p>
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 示例 1：
+ * <p>
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * 示例 2：
+ * <p>
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ *      注意你可以重复使用字典中的单词。
+ * 示例 3：
+ * <p>
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/word-break
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Jun
+ * @date 2020/5/20 下午10:05
+ */
+public class _139单词拆分 {
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        return word_break(s, new HashSet<>(wordDict), 0);
+    }
+
+    private static boolean word_break(String s, Set<String> wordDict, int start) {
+        if (start == s.length()) {
+            return true;
+        }
+        for (int end = start; end < s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) && word_break(s, wordDict, end)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //    public static void main(String[] args) {
+//        String s = "leetcode";
+//        List<String> wordDict = new ArrayList<>();
+//        wordDict.add("leet");
+//        wordDict.add("code");
+//
+//        System.out.println(wordBreak(s, wordDict));
+//    }
+    public static void main(String[] args) {
+        String string = "abc";
+        Set set = new HashSet();
+        set.add("abc");
+        set.add("cdefg");
+        System.out.println(set.contains(string));
+    }
+}
+
