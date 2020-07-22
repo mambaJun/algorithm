7938003461d226ce9edef6fbc3b2941d9a7890db
Jun <wjxajh@foxmail.com>
2020-07-22 11:48:06 +0800

    [add]未完成 13罗马数组转整数;完成 14、21、28

---
+++ java-algorithm/src/com/jun/_13罗马数组转整数/_13罗马数组转整数.java
@@ -0,0 +1,37 @@
+package com.jun._13罗马数组转整数;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Jun
+ * @date 2020/7/22 上午8:03
+ */
+public class _13罗马数组转整数 {
+    public static void main(String[] args) {
+        System.out.println(romanToInt("II"));
+
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int n = s.length();
+        int num = map.get(s.charAt(n - 1));
+
+        for (int i = n - 2; i >= 0; i--) {
+            Integer left = map.get(s.charAt(i));
+            Integer right = map.get(s.charAt(i + 1));
+            num = left < right ? num - left : num + left;
+        }
+
+        return num;
+    }
+}
