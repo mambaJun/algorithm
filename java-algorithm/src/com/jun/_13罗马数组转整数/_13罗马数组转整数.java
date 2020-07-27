e0288d41855219128a64955bfdb4380384d04d26
Jun <wjxajh@foxmail.com>
2020-07-27 10:07:11 +0800

    [add]完成_12整数转罗马数字；_13罗马数组转整数；_101二叉树的最大深度

--- java-algorithm/src/com/jun/_13罗马数组转整数/_13罗马数组转整数.java
+++ java-algorithm/src/com/jun/_13罗马数组转整数/_13罗马数组转整数.java
@@ -14,14 +14,20 @@ public class _13罗马数组转整数 {
     }
 
     public static int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
 
         int n = s.length();
         int num = map.get(s.charAt(n - 1));
