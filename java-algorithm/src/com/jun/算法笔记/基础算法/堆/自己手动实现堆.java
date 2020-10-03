fcb821f00a497c9797c57d1d15b9a065629a7ba2
Jun <wjxajh@foxmail.com>
2020-10-03 10:06:06 +0800

    [add]

---
+++ java-algorithm/src/com/jun/算法笔记/基础算法/堆/自己手动实现堆.java
@@ -0,0 +1,44 @@
+package com.jun.算法笔记.基础算法.堆;
+
+/**
+ * @author Jun
+ * @date 2020/9/19 下午7:29
+ */
+public class 自己手动实现堆 {
+    /*
+    自己手写堆
+        1. 向集合中插入一个数
+        2. 求集合中的最小值
+        3. 删除最小值
+        4. 删除任意一个元素
+        5. 修改任意一个元素
+
+    存储：
+          左儿子：2X
+          右儿子：2X + 1
+     */
+    //
+    private void down(int x) {
+
+    }
+
+    //
+    private void up(int x) {
+
+    }
+
+    private void remove(int x) {
+
+    }
+
+    private void removeK(int k) {
+
+    }
+
+    private void modify(int k) {
+
+    }
+
+
+
+}
