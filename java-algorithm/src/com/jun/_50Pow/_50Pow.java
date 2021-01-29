bb50112c0f8ef82d73db4f87faca2edf3a7c64d6
Jun <wjxajh@foxmail.com>
2021-01-29 22:42:21 +0800

    [add] offer13机器人运动范围 offer16数值的整数次方

--- java-algorithm/src/com/jun/_50Pow/_50Pow.java
+++ java-algorithm/src/com/jun/_50Pow/_50Pow.java
@@ -6,7 +6,8 @@ package com.jun._50Pow;
  */
 public class _50Pow {
     public static void main(String[] args) {
-        System.out.println(myPow(2, 10));
+//        System.out.println(myPow(2, 10));
+        System.out.println(myPow(2.00000, -2147483648));
     }
     /*
         快速幂
@@ -16,20 +17,30 @@ public class _50Pow {
         化为 6561 * 9
         底数每次翻倍
         指数每次折半，如果为奇数，则-1折半，-1直接乘到结果上
+                A^(n/2) * A^(n/2)
+        A^n =
+                A^(n - 1/2) * A^(n - 1/2) * A
     */
 
     public static double myPow(double base, int power) {
-        double res = 1;
+        if (base == 0) return 0.0;
+        if (power == 0) return 1.0;
+
+        // p = -2147483648 的时候，取反时 已经 超出了 int 范围，所以用 long 代替
         long p = power;
+
         if (p < 0) {
             p = -p;
             base = 1 / base;
         }
+
+        double res = 1.0;
+
         while (p > 0) {
-            if (p % 2 == 1) {
-                res = res * base;
+            if ((p & 1) == 1) {
+                res *= base;
             }
-            p = p >> 1;
+            p >>= 1;
             base *= base;
         }
         return res;
